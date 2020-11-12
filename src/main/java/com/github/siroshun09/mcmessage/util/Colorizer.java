86914d8b8131a12d4b5a8598d8433224515d3d10
Siroshun09 <siro.shun8@gmail.com>
2020-11-12 14:44:37 +0900

    [Rewrite] Colorizer

--- src/main/java/com/github/siroshun09/mcmessage/util/Colorizer.java
+++ src/main/java/com/github/siroshun09/mcmessage/util/Colorizer.java
@@ -24,7 +24,9 @@ public final class Colorizer {
 
     private static final String EMPTY = "";
     private static final char COLOR_MARK = '&';
+    private static final String COLOR_MARK_STRING = String.valueOf(COLOR_MARK);
     private static final char COLOR_SECTION = '§';
+    private static final String COLOR_SECTION_STRING = String.valueOf(COLOR_SECTION);
     private static final char HEX_MARK = '#';
     private static final char X = 'x';
     private static final String COLOR_CODES = "0123456789AaBbCcDdEeFfKkLlMmNnOoRr";
@@ -42,39 +44,61 @@ public final class Colorizer {
             return EMPTY;
         }
 
-        char[] b = str.toCharArray();
-        StringBuilder builder = new StringBuilder();
+        if (str.contains(COLOR_MARK_STRING)) {
+            StringBuilder builder = new StringBuilder(str);
+            return colorize(builder).toString();
+        } else {
+            return str;
+        }
+    }
 
-        for (int i = 0; i < b.length; i++) {
-            if (b[i] != COLOR_MARK || b.length == i + 1) {
-                builder.append(b[i]);
-                continue;
-            }
-
-            if (b[i + 1] == HEX_MARK) {
-                if (i + 7 < b.length) {
-                    boolean added = addHexColor(str.substring(i + 1, i + 8), builder);
-
-                    if (added) {
-                        i += 7;
-                    } else {
-                        builder.append(b[i]).append(b[i + 1]);
-                        i++;
-                    }
-                }
-
-                continue;
-            }
-
-            if (-1 < COLOR_CODES.indexOf(b[i + 1])) {
-                builder.append(COLOR_SECTION).append(Character.toLowerCase(b[i + 1]));
-                i++;
-            } else {
-                builder.append(b[i]);
-            }
+    public static @NotNull StringBuilder colorize(StringBuilder builder) {
+        if (builder == null) {
+            return new StringBuilder();
         }
 
-        return builder.toString();
+        int i = builder.indexOf(COLOR_MARK_STRING);
+
+        if (i == -1) {
+            return builder;
+        }
+
+        int l = builder.length();
+
+        while (i != -1 && i + 1 < l) {
+            if (builder.charAt(i + 1) == HEX_MARK) {
+                if (i + 7 < l) {
+                    String hex = builder.substring(i + 2, i + 8);
+
+                    try {
+                        Integer.parseInt(hex, 16);
+                    } catch (NumberFormatException e) {
+                        i = builder.indexOf(COLOR_MARK_STRING, i + 1);
+                        continue;
+                    }
+
+                    builder.setCharAt(i, COLOR_SECTION);
+                    builder.setCharAt(i + 1, X);
+
+                    for (int j = i + 7; j != i + 1; j--) {
+                        builder.insert(j, COLOR_SECTION_STRING);
+                    }
+
+                    l = builder.length();
+                }
+
+                i = builder.indexOf(COLOR_MARK_STRING, i + 1);
+                continue;
+            }
+
+            if (-1 < COLOR_CODES.indexOf(builder.charAt(i + 1))) {
+                builder.setCharAt(i, COLOR_SECTION);
+            }
+
+            i = builder.indexOf(COLOR_MARK_STRING, i + 1);
+        }
+
+        return builder;
     }
 
     /**
@@ -102,27 +126,5 @@ public final class Colorizer {
         }
 
         return pattern.matcher(str).replaceAll("");
-    }
-
-    private static boolean addHexColor(String hex, StringBuilder builder) {
-        char[] array = hex.toCharArray();
-
-        if (array.length != 7 || array[0] != HEX_MARK) {
-            return false;
-        }
-
-        try {
-            Integer.parseInt(hex.substring(1), 16);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-
-        builder.append(COLOR_SECTION).append(X);
-
-        for (char c : hex.substring(1).toCharArray()) {
-            builder.append(COLOR_SECTION).append(c);
-        }
-
-        return true;
     }
 }

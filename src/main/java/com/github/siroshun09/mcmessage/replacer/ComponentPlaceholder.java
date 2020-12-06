0a8e4ae1dcd47f757ced8293853c83d340a8e44d
Siroshun09 <siro.shun8@gmail.com>
2020-12-07 06:16:08 +0900

    [Add] ComponentPlaceholder

---
+++ src/main/java/com/github/siroshun09/mcmessage/replacer/ComponentPlaceholder.java
@@ -0,0 +1,52 @@
+/*
+ *     Copyright 2020 Siroshun09
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package com.github.siroshun09.mcmessage.replacer;
+
+import com.github.siroshun09.mcmessage.util.Colorizer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Function;
+
+public interface ComponentPlaceholder<T, C extends Component> extends FunctionalPlaceholder<T> {
+
+    static <T, C extends Component> ComponentPlaceholderImpl<T, C> create(@NotNull String placeholder, @NotNull Function<T, C> function) {
+        return new ComponentPlaceholderImpl<>(placeholder, function);
+    }
+
+    @NotNull C createComponent(T value);
+
+    @Override
+    @NotNull
+    default Function<T, String> getFunction() {
+        return v -> {
+            var component = createComponent(v);
+            var serialized = LegacyComponentSerializer.legacySection().serialize(component);
+            return Colorizer.colorize(serialized);
+        };
+    }
+
+    @Override
+    default @NotNull TextReplacementConfig toTextReplacementConfig(@NotNull T value) {
+        return TextReplacementConfig.builder()
+                .match(getPlaceholder())
+                .replacement(createComponent(value))
+                .build();
+    }
+}

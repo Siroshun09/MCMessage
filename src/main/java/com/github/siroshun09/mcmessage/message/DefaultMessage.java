6d408f49103061736da088a1951d427928dc1bb4
Siroshun09 <siro.shun8@gmail.com>
2020-12-07 13:36:19 +0900

    [Add] DefaultMessage#of

--- src/main/java/com/github/siroshun09/mcmessage/message/DefaultMessage.java
+++ src/main/java/com/github/siroshun09/mcmessage/message/DefaultMessage.java
@@ -16,6 +16,8 @@
 
 package com.github.siroshun09.mcmessage.message;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -29,6 +31,11 @@ public interface DefaultMessage extends KeyedMessage {
     static DefaultMessage of(@NotNull String key, @NotNull Message message) {
         Objects.requireNonNull(message);
         return new DefaultMessageImpl(key, message.get());
+    }
+
+    static DefaultMessage of(@NotNull String key, @NotNull Component component) {
+        Objects.requireNonNull(component);
+        return new DefaultMessageImpl(key, LegacyComponentSerializer.legacyAmpersand().serialize(component));
     }
 
     default @NotNull String getDefault() {

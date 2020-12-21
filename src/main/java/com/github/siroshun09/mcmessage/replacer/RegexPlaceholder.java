b3c976a035e718403d5d9298309c04df347077a9
Siroshun09 <siro.shun8@gmail.com>
2020-12-22 03:39:30 +0900

    [Add] Javadoc

--- src/main/java/com/github/siroshun09/mcmessage/replacer/RegexPlaceholder.java
+++ src/main/java/com/github/siroshun09/mcmessage/replacer/RegexPlaceholder.java
@@ -24,31 +24,70 @@ import java.util.Objects;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+/**
+ * An interface that represents placeholder using regex.
+ */
 public interface RegexPlaceholder extends Placeholder {
 
+    /**
+     * Creates new {@link RegexPlaceholder}
+     *
+     * @param pattern a {@link Pattern}
+     * @return new {@link RegexPlaceholder}
+     */
     @Contract(value = "_ -> new", pure = true)
     static @NotNull RegexPlaceholder create(@NotNull Pattern pattern) {
         return new RegexPlaceholderImpl(pattern);
     }
 
+    /**
+     * Creates new {@link RegexPlaceholder}
+     *
+     * @param pattern a regex string to compile to {@link Pattern}
+     * @return new {@link RegexPlaceholder}
+     * @throws PatternSyntaxException if the expression's syntax is invalid
+     */
     @Contract(value = "_ -> new", pure = true)
     static @NotNull RegexPlaceholder create(@NotNull String pattern) throws PatternSyntaxException {
         Objects.requireNonNull(pattern);
         return create(Pattern.compile(pattern));
     }
 
+    /**
+     * Gets the {@link Pattern}
+     *
+     * @return the {@link Pattern}
+     */
     @NotNull Pattern getPattern();
 
+    /**
+     * Gets the regex string.
+     *
+     * @return the regex string
+     * @see Pattern#toString()
+     */
     @Override
     default @NotNull String getPlaceholder() {
         return getPattern().toString();
     }
 
+    /**
+     * Creates {@link RegexPlaceholder} with the replacement string.
+     *
+     * @param replacement a replacement string
+     * @return new {@link RegexPlaceholder}
+     */
     @Override
     default @NotNull Replacer toReplacer(@NotNull String replacement) {
         return RegexReplacer.create(getPattern(), replacement);
     }
 
+    /**
+     * Creates {@link TextReplacementConfig} with the replacement string.
+     *
+     * @param replacement a replacement string
+     * @return new {@link TextReplacementConfig}
+     */
     @Override
     default @NotNull TextReplacementConfig toTextReplacementConfig(@NotNull String replacement) {
         return TextReplacementConfig.builder()
